be1742af19a2a161189c6db96019a25603b8d9fd
thosewhoknow <chrisbalshaw5@icloud.com>
2025-05-09 17:16:26 -0500

    fwafaf

--- ElysianV.lua
+++ ElysianV.lua
@@ -1,11 +1,11 @@
 local placeId = game.PlaceId
 
 if placeId == 18687417158 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Selivus08/forsakenfr/refs/heads/main/forsaken.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Selivus08/Elysian/refs/heads/main/forsaken.lua",true))()
 elseif placeId == 71895508397153 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Selivus08/forsakenfr/refs/heads/main/dieofdeath.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Selivus08/Elysian/refs/heads/main/dieofdeath.lua",true))()
 else
 _G.Config = { ["Theme"] = "spotify" }
-local Notifs = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Selivus08/forsakenfr/refs/heads/main/notification.lua", true))();
+local Notifs = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Selivus08/Elysian/refs/heads/main/notification.lua", true))();
 Notifs:Notify(nil, "this game is not supported by elysian", "error", 3);
 end

2a74ab475ca8e9b1f3687562678f833d2b550bce
thosewhoknow <chrisbalshaw5@icloud.com>
2025-05-30 02:34:39 -0500

    Update ElysianV.lua

--- ElysianV.lua
+++ ElysianV.lua
@@ -4,6 +4,10 @@ if placeId == 18687417158 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Selivus08/Elysian/refs/heads/main/forsaken.lua",true))()
 elseif placeId == 71895508397153 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Selivus08/Elysian/refs/heads/main/dieofdeath.lua",true))()
+elseif placeId == 16483433878 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Selivus08/Elysian/refs/heads/main/blocktales.lua",true))()
+elseif placeId == 13559635034 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Selivus08/Elysian/refs/heads/main/combatinitiation.lua",true))()
 else
 _G.Config = { ["Theme"] = "spotify" }
 local Notifs = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Selivus08/Elysian/refs/heads/main/notification.lua", true))();
